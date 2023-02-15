685c8e72f9ffa8e7fc253e4ffe2a4ea738f50571
MiixZ <xwarzmiixz@gmail.com>
2023-02-15 15:51:20 +0100

    abilityUser documentation

--- src/main/java/abilitieSystem/abilityUser.java
+++ src/main/java/abilitieSystem/abilityUser.java
@@ -2,12 +2,6 @@ package abilitieSystem;
 
 import org.bukkit.entity.Player;
 import fruitSystem.devilFruit;
-import castSystem.coolDown;
-import abilitieSystem.abilities;
-import abilitieSystem.df;
-import abilitieSystem.haki;
-
-
 import org.bukkit.Bukkit;
 import org.bukkit.event.entity.*;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -20,13 +14,24 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 
 import java.util.ArrayList;
 
-public class abilityUser{
-    private String playerName;
+
+/**
+ * @brief Abilities' users for fruits Class.
+ * @author Vaelico786
+ */
+
+public class abilityUser {
+    private final String playerName;
     public int actual;
     private devilFruit fruit;
     private abilities dfAbilities, hakiAbilities;
 
-    public abilityUser(String playerName){
+    /**
+     * @brief abilityUser constructor.
+     * @param playerName Player's name.
+     * @author Vaelico786.
+     */
+    public abilityUser(String playerName) {
         this.playerName = playerName;
         this.dfAbilities = null;
         this.fruit = null;
@@ -35,97 +40,87 @@ public class abilityUser{
         actual = 0;
     }
 
-    public Player getPlayer(){
+    // *********************************************** DEVIL FRUIT *******************************************************
+
+    /**
+     * @brief Gets the actual player.
+     * @author Vaelico786.
+     */
+    public Player getPlayer() {
         if(Bukkit.getPlayerExact(playerName) == null)
             return null;
 
         return Bukkit.getPlayerExact(playerName);
     }
 
-    public String getName(){
+    /**
+     * @brief Gets the actual player's name.
+     * @author Vaelico786.
+     */
+    public String getPlayerName() {
         return playerName;
     }
 
-    
-    
-    public String getPlayerName(){
-        return playerName;
-    }
-
-    public void setFruit(devilFruit fruit, abilities dfAbilities){
+    /**
+     * @brief Set the actual player's fruit.
+     * @author Vaelico786.
+     */
+    public void setFruit(devilFruit fruit, abilities dfAbilities) {
         this.fruit = fruit;
         this.dfAbilities = dfAbilities;
         dfAbilities.user=this;
     }
-    
-    public void removeFruit(){
+
+    /**
+     * @brief Gets the actual player's fruit.
+     * @author Vaelico786.
+     */
+    public devilFruit getFruit() {
+        return fruit;
+    }
+
+    /**
+     * @brief Gets the actual player fruit's abilities.
+     * @author Vaelico786.
+     */
+    public df getDFAbilities() {
+        return (df) dfAbilities;
+    }
+
+    /**
+     * @brief Returns true if player has a fruit.
+     * @author Vaelico786.
+     */
+    public boolean hasFruit() {
+        return fruit != null;
+    }
+
+    /**
+     * @brief Removes the player's fruit.
+     * @author Vaelico786.
+     */
+    public void removeFruit() {
         this.fruit = null;
         this.dfAbilities = null;
     }
 
-    public void setHaki(abilities hakiAbilities){
-        this.hakiAbilities = hakiAbilities;
-        this.hakiAbilities.user=this;
-    }
-
-    public devilFruit getFruit(){
-        return fruit;
-    }
-
-    public df getDFAbilities(){
-        return (df) dfAbilities;
-    }
-
-    public haki getHakiAbilities(){
-        return (haki) hakiAbilities;
-    }
-
-    public boolean hasFruit(){
-        return fruit != null;
-    }
-
-    public boolean hasHaki(){
-        return hakiAbilities != null;
-    }
-
-    public int getHakiLevel(){
-        if(hakiAbilities == null)
-            return 0;
-
-        return getHakiAbilities().getLevel();
-    }
-
-    public void setHakiLevel(int level){
-        if(hakiAbilities == null)
-            return;
-
-        getHakiAbilities().setLevel(level);
-    }
-
-    public double getHakiExp(){
-        if(hakiAbilities == null)
-            return 0;
-
-        return getHakiAbilities().getExp();
-    }
-
-    public void setHakiExp(double exp){
-        if(hakiAbilities == null)
-            return;
-
-        getHakiAbilities().setExp(exp);
-    }
-
+    /**
+     * @brief Switch the current ability in game (scoreboard).
+     * @author Vaelico786.
+     */
     public void switchAbility() {
         if (dfAbilities == null)
             return;
         actual = (actual + 1) % getDFAbilities().getAbilitiesNames().size();
-
     }
 
-    public int abilityActive() {
+    /**
+     * @brief Activates the current ability selected on scoreboard.
+     * @author Vaelico786.
+     */
+    public void abilityActive() {
         if (dfAbilities == null)
-            return -1;
+            return;
         if (getDFAbilities().active) {
             switch (actual) {
                 case 0:
@@ -148,92 +143,245 @@ public class abilityUser{
                     break;
             }
         }
-        return dfAbilities.abilitiesCD.get(actual);
     }
 
+    /**
+     * @brief Gets all abilities' CDs of the current devil fruit.
+     * @author Vaelico786.
+     */
     public ArrayList<Integer> getAbilitiesCD() {
         if (dfAbilities == null)
             return null;
         return dfAbilities.abilitiesCD;
     }
 
+    /**
+     * @brief Gets all abilities' names of the current devil fruit.
+     * @author Vaelico786.
+     */
     public ArrayList<String> getAbilitiesNames() {
         if (dfAbilities == null)
             return null;
         return dfAbilities.abilitiesNames;
     }
 
-    public void onEntityDamage(EntityDamageEvent event){
+    // *********************************************** HAKI *******************************************************
+
+    /**
+     * @brief Sets Haki abilities' kit.
+     * @author Vaelico786.
+     */
+    public void setHaki(abilities hakiAbilities) {
+        this.hakiAbilities = hakiAbilities;
+        this.hakiAbilities.user = this;
+    }
+
+    /**
+     * @brief Gets Haki abilities' kit.
+     * @author Vaelico786.
+     */
+    public haki getHakiAbilities() {
+        return (haki) hakiAbilities;
+    }
+
+    /**
+     * @brief Returns true if actual player has haki.
+     * @author Vaelico786.
+     */
+    public boolean hasHaki() {
+        return hakiAbilities != null;
+    }
+
+    /**
+     * @brief Gets the actual player haki's level.
+     * @author Vaelico786.
+     */
+    public int getHakiLevel() {
+        if(hakiAbilities == null)
+            return 0;
+
+        return getHakiAbilities().getLevel();
+    }
+
+    /**
+     * @brief Sets the actual player haki's level.
+     * @author Vaelico786.
+     */
+    public void setHakiLevel(int level) {
+        if(hakiAbilities == null)
+            return;
+
+        getHakiAbilities().setLevel(level);
+    }
+
+    /**
+     * @brief Returns the haki abilities' experience.
+     * @author Vaelico786.
+     */
+    public double getHakiExp() {
+        if(hakiAbilities == null)
+            return 0;
+
+        return getHakiAbilities().getExp();
+    }
+
+    /**
+     * @brief Sets the haki abilities' experience.
+     * @author Vaelico786.
+     */
+    public void setHakiExp(double exp) {
+        if(hakiAbilities == null)
+            return;
+
+        getHakiAbilities().setExp(exp);
+    }
+
+    // *********************************************** PASSIVES *******************************************************
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onEntityDamage(EntityDamageEvent) 
+     * @author Vaelico786.
+     */
+    public void onEntityDamage(EntityDamageEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onEntityDamage(event);
         if(dfAbilities != null)
             dfAbilities.onEntityDamage(event);
     }
-    public void onPlayerDeath(PlayerDeathEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onPlayerDeath(PlayerDeathEvent)
+     * @author Vaelico786.
+     */
+    public void onPlayerDeath(PlayerDeathEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onPlayerDeath(event);
         if(dfAbilities != null)
             dfAbilities.onPlayerDeath(event);
     }
-    public void playerOnWater(PlayerMoveEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#playerOnWater(PlayerMoveEvent)
+     * @author Vaelico786.
+     */
+    public void playerOnWater(PlayerMoveEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.playerOnWater(event);
         if(dfAbilities != null)
             dfAbilities.playerOnWater(event);
     }
-    public void onPlayerToggleSneak(PlayerToggleSneakEvent e){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onPlayerToggleSneak(PlayerToggleSneakEvent)
+     * @author Vaelico786.
+     */
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent e) {
         if(hakiAbilities != null)
             hakiAbilities.onPlayerToggleSneak(e);
         if(dfAbilities != null)
             dfAbilities.onPlayerToggleSneak(e);
     }
-    public void onFall(EntityDamageEvent e){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onFall(EntityDamageEvent)
+     * @author Vaelico786.
+     */
+    public void onFall(EntityDamageEvent e) {
         if(hakiAbilities != null)
             hakiAbilities.onFall(e);
         if(dfAbilities != null)
             dfAbilities.onFall(e);
     }
-    public void onPlayerItemConsume(PlayerItemConsumeEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onPlayerItemConsume(PlayerItemConsumeEvent)
+     * @author Vaelico786.
+     */
+    public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onPlayerItemConsume(event);
         if(dfAbilities != null)
             dfAbilities.onPlayerItemConsume(event);
     }
-    public void onEntityPickupItem(EntityPickupItemEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onEntityPickupItem(EntityPickupItemEvent)
+     * @author Vaelico786.
+     */
+    public void onEntityPickupItem(EntityPickupItemEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onEntityPickupItem(event);
         if(dfAbilities != null)
             dfAbilities.onEntityPickupItem(event);
     }
-    public void onPlayerEggThrow(PlayerEggThrowEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onPlayerEggThrow(PlayerEggThrowEvent)
+     * @author Vaelico786.
+     */
+    public void onPlayerEggThrow(PlayerEggThrowEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onPlayerEggThrow(event);
         if(dfAbilities != null)
             dfAbilities.onPlayerEggThrow(event);
     }
-    public void onEntityShootBow(EntityShootBowEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onEntityShootBow(EntityShootBowEvent)
+     * @author Vaelico786.
+     */
+    public void onEntityShootBow(EntityShootBowEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onEntityShootBow(event);
         if(dfAbilities != null)
             dfAbilities.onEntityShootBow(event);
     }
-    public void onInventoryClick(InventoryClickEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onInventoryClick(InventoryClickEvent)
+     * @author Vaelico786.
+     */
+    public void onInventoryClick(InventoryClickEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onInventoryClick(event);
         if(dfAbilities != null)
             dfAbilities.onInventoryClick(event);
     }
 
-    public void onPlayerRespawn(PlayerRespawnEvent event){
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onPlayerRespawn(PlayerRespawnEvent)
+     * @author Vaelico786.
+     */
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
         if(hakiAbilities != null)
             hakiAbilities.onPlayerRespawn(event);
     }
 
-    public void onUserDamageAnotherEntity(EntityDamageByEntityEvent event){
+    /**
+     * @brief Passive function when a player deals damage to another entity.
+     * @author Vaelico786.
+     */
+    public void onUserDamageAnotherEntity(EntityDamageByEntityEvent event) {
         if(hasHaki()) getHakiAbilities().onUserDamageAnotherEntity(event);
     }
-    public void onItemDamage(PlayerItemDamageEvent event){
+
+    /**
+     * @brief Same as "abilities" class.
+     * @see abilities#onItemDamage(PlayerItemDamageEvent)
+     * @author Vaelico786.
+     */
+    public void onItemDamage(PlayerItemDamageEvent event) {
         if(hasHaki()) getHakiAbilities().onItemDamage(event);
     }
-
 }
